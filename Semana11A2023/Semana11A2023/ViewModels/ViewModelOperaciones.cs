1cdec5770ab5f935a3023581dda09f9984e23c9c
AldairAcosta1523 <aldairacosta76@gmail.com>
2023-06-03 00:32:17 -0500

    first commit

---
+++ Semana11A2023/Semana11A2023/ViewModels/ViewModelOperaciones.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Semana11A2023.ViewModels
+{
+    public class ViewModelOperaciones : ViewModelBase 
+    {
+
+        double valorA;
+        public double ValorA
+        {
+            get { return valorA; }
+            set
+            {
+                if (valorA != value)
+                {
+                    valorA = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        double valorB;
+        public double ValorB
+        {
+            get { return valorB; }
+            set
+            {
+                if (valorB != value)
+                {
+                    valorB = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        double resultadoResta;
+        public double ResultadoResta
+        {
+            get { return resultadoResta; }
+            set
+            {
+                if (resultadoResta != value)
+                {
+                    resultadoResta = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        double resultadoSuma;
+        public double ResultadoSuma
+        {
+            get { return resultadoSuma; }
+            set
+            {
+                if (resultadoSuma != value)
+                {
+                    resultadoSuma = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        double resultadoMultplicacion;
+        public double ResultadoMultiplicar
+        {
+            get { return resultadoMultplicacion; }
+            set
+            {
+                if (resultadoMultplicacion != value)
+                {
+                    resultadoMultplicacion = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        double resultadoDividir;
+        public double ResultadoDividir
+        {
+            get { return resultadoDividir; }
+            set
+            {
+                if (resultadoDividir != value)
+                {
+                    resultadoDividir = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string mensajeError;
+        public string MensajeError
+        {
+            get { return mensajeError; }
+            set
+            {
+                mensajeError = value;
+                OnPropertyChanged(nameof(MensajeError));
+            }
+        }
+        public ICommand Sumar { protected set; get; }
+        public ICommand Restar { protected set; get; }
+        public ICommand Multiplicar { protected set; get; }
+        public ICommand Dividir { protected set; get; }
+
+
+
+        public ViewModelOperaciones()
+        {
+            Restar = new Command(() =>
+            {
+                ResultadoResta = ValorA - ValorB;
+            });
+            Sumar = new Command(() =>
+            {
+                ResultadoSuma = ValorA + ValorB;
+            });
+            Multiplicar = new Command(() =>
+            {
+                ResultadoMultiplicar = ValorA * ValorB;
+            });
+            Dividir = new Command(() =>
+            {
+                if (ValorB != 0 && ValorA != 0)
+                {
+                    ResultadoDividir = ValorA / ValorB;
+                    MensajeError = string.Empty; ;
+                }
+                else
+                {
+                    MensajeError = "Error: No se puede dividir entre cero.";
+                }
+            })
+            {
+
+            };
+        }
+    }
+}
---
+++ Semana11A2023/Semana11A2023/Views/Operacions.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Semana11A2023.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana11A2023.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Operacions : TabbedPage
+    {
+        public Operacions()
+        {
+            InitializeComponent();
+            {
+                Children.Add(new Suma());
+                Children.Add(new Resta());
+                Children.Add(new Multiplicacion());
+                Children.Add(new Division());
+            };
+        }
+    }
+}
